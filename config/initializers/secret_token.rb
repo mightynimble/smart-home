3317de3dcf5f23d154f67ddaba007c92b1e7a1b0
Bo Sun <bosun58@gmail.com>
2013-05-20 23:17:47 -0400

    Rails project initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartHome::Application.config.secret_token = '29ac249b4991f2edd79962573fc7404e178dbad22d44145221a8bf4391b73539db87a5109a06a241305e9cb0a08a6d577bdde6a81e87c5946b1148f8bbd70c74'
