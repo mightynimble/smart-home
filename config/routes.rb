91c423b0edb649e6e5c653395dc6e0d7f89bf3c2
Bo Sun <bosun58@gmail.com>
2013-05-24 13:16:37 -0400

    removed .idea folder from git repo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 SmartHome::Application.routes.draw do
 
+  get "session/new"
+
+  get "session/create"
+
+  get "session/destroy"
+
+  get '/login' => 'sessions#new'
 
   resources :users
 
-
+  root :to => 'sessions#new'
 end
