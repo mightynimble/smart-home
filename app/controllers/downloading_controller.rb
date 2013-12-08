b9fd74f8e1039595574c03a793c99d39b45d290a
Bo Sun <bosun58@gmail.com>
2013-12-08 14:48:25 -0500

    Added methods in downloading controller for ajax callbacks.

--- app/controllers/downloading_controller.rb
+++ app/controllers/downloading_controller.rb
@@ -1,4 +1,23 @@
 class DownloadingController < ApplicationController
-  def index
+  respond_to :json, :only => [:all, :active]
+
+  def active
+    torrents = Utorrent.new
+    filtered_results = nil
+    torrents.list['torrents'].each do |item|
+      unless torrents.status(item).eql? 'completed'
+        filtered_results << item
+      end
+    end
+    respond_with [filtered_results]
+  end
+
+  def all
+    torrents = Utorrent.new
+    filtered_results = nil
+    torrents.list['torrents'].each do |item|
+      filtered_results << item
+    end
+    respond_with [filtered_results]
   end
 end
